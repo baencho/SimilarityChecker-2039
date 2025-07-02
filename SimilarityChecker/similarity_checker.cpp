0f5d9f5149546842a1b3e1ac8420abe169767293
baencho <yuen6482@gmail.com>
2025-07-02 15:29:03 +0900

    Add LengthPoint function for same length strings

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -1,3 +1,9 @@
-class SimilarityChecker {
+#include <string>
+using std::string;
 
+class SimilarityChecker {
+public:
+	int getLengthPoint(string str1, string str2) {
+		if (str1.length() == str2.length()) return 60;
+	}
 };
--- SimilarityChecker/similarity_checker_test.cpp
+++ SimilarityChecker/similarity_checker_test.cpp
@@ -1,6 +1,14 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.cpp"
 
+TEST(TS, TC1) {
+	SimilarityChecker checker;
+	string str1 = "ABC";
+	string str2 = "ABC";
+	int actual = checker.getLengthPoint(str1, str2);
+	EXPECT_EQ(60, actual);
+
+}
 int main()
 {
 	::testing::InitGoogleMock();

dc3cba1448d26b351a084cee8f95edb38ddaa187
baencho <yuen6482@gmail.com>
2025-07-02 15:59:04 +0900

    [Refactor] Change UT Fixture name

--- SimilarityChecker/similarity_checker_test.cpp
+++ SimilarityChecker/similarity_checker_test.cpp
@@ -1,7 +1,7 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.cpp"
 
-class TCFixture : public testing::Test {
+class LengthCheckerFixture : public testing::Test {
 public:
 	SimilarityChecker checker;
 	void checkLengthPoint(const string& str1, const string& str2, int expected) {
@@ -10,19 +10,19 @@ public:
 	}
 };
 
-TEST_F(TCFixture, TC1) {
+TEST_F(LengthCheckerFixture, TC1) {
 	checkLengthPoint("ABC", "ABC", 60);
 }
 
-TEST_F(TCFixture, TC2) {
+TEST_F(LengthCheckerFixture, TC2) {
 	checkLengthPoint("ABC", "ABCDE", 36);
 }
 
-TEST_F(TCFixture, TC3) {
+TEST_F(LengthCheckerFixture, TC3) {
 	checkLengthPoint("NLKISGDLIE", "ABCDE", 30);
 }
 
-TEST_F(TCFixture, EmptyString1) {
+TEST_F(LengthCheckerFixture, EmptyString1) {
 	try {
 		checker.checkEmptyString("", "ABC");
 		FAIL();
@@ -32,7 +32,7 @@ TEST_F(TCFixture, EmptyString1) {
 	}
 }
 
-TEST_F(TCFixture, EmptyString2) {
+TEST_F(LengthCheckerFixture, EmptyString2) {
 	const int INVALID_POINT = 0;
 	try {
 		checkLengthPoint("", "ABC", INVALID_POINT);
